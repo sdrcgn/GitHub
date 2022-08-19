80350e9dbebed74939d6a356532080b65ce1af50
Sidar Cetinkaya <sidar457@icloud.com>
2022-08-18 23:19:49 -0400

    coming

--- src/day1/HelloClass.java
+++ src/day1/HelloClass.java
@@ -3,6 +3,7 @@ package day1;
 public class HelloClass {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("local repo");
 
 
     // i started
